cea7e141efb5393bfcb21c748fd45de4d69127da
Coty Condry <ccondry@cisco.com>
2020-05-19 12:09:56 -0500

    replace axios with node-fetch

--- client/utils/index.js
+++ client/utils/index.js
@@ -1,15 +1,24 @@
-import axios from 'axios'
+function addUrlQueryParams (endpoint, params) {
+  let url = new URL(endpoint)
+  if (params) {
+    // append URL query paramenters
+    Object.keys(params).forEach(key => {
+      url.searchParams.append(key, params[key])
+    })
+  }
+  return url
+}
 
 export const load = async function (jwt, endpoint, query) {
   try {
-    console.log('GET', endpoint)
-    const options = {
-      params: query
-    }
-    options.headers = {
-      Authorization: 'Bearer ' + jwt
-    }
-    return await axios.get(endpoint, options)
+    const url = addUrlQueryParams(endpoint, query)
+    console.log('GET', url)
+    return window.fetch(url, {
+      method: 'GET',
+      headers: {
+        Authorization: 'Bearer ' + jwt
+      }
+    })
   } catch (e) {
     throw e
   }
@@ -17,13 +26,15 @@ export const load = async function (jwt, endpoint, query) {
 
 export const put = async function (jwt, endpoint, query, data) {
   try {
-    const options = {
-      params: query
-    }
-    options.headers = {
-      Authorization: 'Bearer ' + jwt
-    }
-    return await axios.put(endpoint, data, options)
+    const url = addUrlQueryParams(endpoint, query)
+    console.log('PUT', url, data)
+    return window.fetch(url, {
+      method: 'PUT',
+      headers: {
+        Authorization: 'Bearer ' + jwt
+      },
+      body: JSON.stringify(data)
+    })
   } catch (e) {
     throw e
   }
@@ -31,13 +42,15 @@ export const put = async function (jwt, endpoint, query, data) {
 
 export const post = async function (jwt, endpoint, query, data) {
   try {
-    const options = {
-      params: query
-    }
-    options.headers = {
-      Authorization: 'Bearer ' + jwt
-    }
-    return await axios.post(endpoint, data, options)
+    const url = addUrlQueryParams(endpoint, query)
+    console.log('POST', url, data)
+    return window.fetch(url, {
+      method: 'POST',
+      headers: {
+        Authorization: 'Bearer ' + jwt
+      },
+      body: JSON.stringify(data)
+    })
   } catch (e) {
     throw e
   }
@@ -45,13 +58,15 @@ export const post = async function (jwt, endpoint, query, data) {
 
 export const httpDelete = async function (jwt, endpoint, query) {
   try {
-    const options = {
-      params: query
-    }
-    options.headers = {
-      Authorization: 'Bearer ' + jwt
-    }
-    return await axios.delete(endpoint, options)
+    const url = addUrlQueryParams(endpoint, query)
+    console.log('DELETE', url, data)
+    return window.fetch(url, {
+      method: 'DELETE',
+      headers: {
+        Authorization: 'Bearer ' + jwt
+      },
+      body: JSON.stringify(data)
+    })
   } catch (e) {
     throw e
   }

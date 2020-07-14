761f869913fc8534cdafc8d51548bd6d084af6dc
Coty Condry <ccondry@cisco.com>
2020-07-13 19:22:54 -0500

    reduce REST request logging

--- client/utils/index.js
+++ client/utils/index.js
@@ -1,7 +1,7 @@
 function addUrlQueryParams (endpoint, params) {
-  console.log('addUrlQueryParams:', endpoint, params)
+  // console.log('addUrlQueryParams:', endpoint, params)
   let url = new window.URL(endpoint)
-  console.log('new URL object =', url)
+  // console.log('new URL object =', url)
   if (params) {
     // append URL query paramenters
     Object.keys(params).forEach(key => {
@@ -13,9 +13,9 @@ function addUrlQueryParams (endpoint, params) {
 
 async function rest (method, jwt, endpoint, query, data) {
   try {
-    console.log('REST request:', method, endpoint, query, data)
+    // console.log('REST request:', method, endpoint, query, data)
     const url = addUrlQueryParams(endpoint, query)
-    console.log(method, url, data)
+    // console.log(method, url, data)
     let body
     if (data) {
       body = JSON.stringify(data)
